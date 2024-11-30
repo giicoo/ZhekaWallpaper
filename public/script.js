ab10189a7d030887d915a8c7694ce1d4bf130b82
giicoo <arnst.artyom@gmail.com>
2024-11-30 12:24:24 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,92 @@
+const socket = new WebSocket('ws://localhost:3000');
+
+socket.addEventListener('open', () => {
+    console.log('Connected to the server');
+    socket.send(JSON.stringify({ type: 'greeting', message: 'Hello, server!' }));
+});
+
+socket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
+  console.log('Message from server:', data);
+});
+
+socket.addEventListener('error', (error) => {
+  console.error('WebSocket error:', error);
+});
+
+
+socket.addEventListener('close', () => {
+  console.log('Connection closed');
+});
+
+
+function updateWeather() {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=Tula,ru&appid=5b2c84c4859a9efb2401c9c9fec0fb6f",
+        type: "GET",
+        crossDomain: true,
+        data: "",
+        dataType: "json",
+        success: function (response) {
+            
+                var resp = JSON.parse(JSON.stringify(response));
+
+            $("#temp").text((resp.main.temp).toFixed(2)+"К");
+            $("#weath").text(resp.weather[0].main);
+            console.log();
+            console.log(resp.main.temp)
+
+            
+        },
+        error: function (xhr, status) {
+                
+            fetch('./HanyiSentyPagoda.ttf')
+            .then(resp => resp.arrayBuffer())
+            .then(font => {
+                const fontFace = new FontFace('HanyiSentyPagoda', font);
+                document.fonts.add(fontFace);
+                
+            })
+           
+            $("#weath").text("万口人乚山口认");
+            $("#temp").text("冂凵匚扣卄");
+            $("#weath").css("font-family", "HanyiSentyPagoda");
+            $("#temp").css("font-family", "HanyiSentyPagoda");
+
+
+            
+        }
+    });
+} setInterval(updateWeather, 3.6e+6);
+
+function updateTime() { 
+    const now = new Date(); 
+    const hours = now.getHours().toString().padStart(2, '0'); 
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const date = now.getDate().toString();
+    const month = now.getMonth() + 1;
+    month.toString();
+    const year = now.getUTCFullYear().toString();
+    document.getElementById('time').textContent = `${hours}:${minutes}`; 
+    document.getElementById('Date_month').textContent = `${date}.${month}.${year}`;
+} setInterval(updateTime, 1000);
+
+updateWeather();
+
+
+$(document).ready(function(){
+    $('#but').click(function() {
+        generateTip();
+    }); 
+  });
+
+  function generateTip() {
+    socket.send(JSON.stringify({ type: 'greeting', message: 'Button click' }));
+  }
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+   // Обработка входящих сообщений от клиента
+    ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    console.log('Received from client:', data);
+
+    // Пример: отправляем ответ обратно клиенту
+    if (data.type === 'click') {
+      ws.send(JSON.stringify({ message: 'Click received!', x: data.x, y: data.y }));
+    }
+  });
+
+  ws.on('close', () => {
+    clearInterval(interval);
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
